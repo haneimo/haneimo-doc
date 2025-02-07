fffbdecfee7690d73f86f5ccad43ff66979052f1
haneimo <haneimo@gmail.com>
2025-02-07 21:18:45 +0900

    サイトを自サーバ公開用に改変

---
+++ readme.md
@@ -0,0 +1,18 @@
+```
+python3 -m venv .venv
+source .venv/bin/activate
+pip install mkdocs
+```
+
+```
+mkdocs --version
+mkdocs new .
+```
+
+```
+mkdocs serve
+```
+
+```
+mkdocs build
+```
---
+++ site/js/custom.js
@@ -0,0 +1,9 @@
+// filepath: /home/haneimo/workspace/haneimo-doc/js/custom.js
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3874515773493908";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log("load ads");
+})();
